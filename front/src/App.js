f151761167cfe97fff5de3927cb13a14a9f5df05
Your Name <you@example.com>
2025-01-30 03:47:14 +0900

    eventSection update

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./css/styles.css";
+import axios from 'axios';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./component/login";
@@ -9,15 +10,33 @@ import Forgotid from "./component/Forgotid";
 import Forgotpw from "./component/Forgotpw";
 
 const App = () => {
+  const [user, setUser] = useState(null); // 유저 정보
+
+  useEffect(() => {
+    const userInfo = sessionStorage.getItem('user');
+    if (userInfo) {
+      setUser(JSON.parse(userInfo));
+    } else {
+      axios.get('/api/auth/userinfo', { withCredentials: true })
+        .then((response) => {
+          setUser(response.data);
+          sessionStorage.setItem('user', JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          console.error('유저 정보 가져오기 실패', error);
+        });
+    }
+  }, []);
+
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/Signup" element={<Signup />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="/Forgotid" element={<Forgotid />} />
-      <Route path="/Forgotpw" element={<Forgotpw />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Main user={user}/>} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Forgotid" element={<Forgotid />} />
+        <Route path="/Forgotpw" element={<Forgotpw />} />
+      </Routes>
     </Router>
   );
 };
